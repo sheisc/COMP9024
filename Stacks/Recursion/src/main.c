d1c64c359d855351a5efb0e72dbc93b4e9286eb8
iron <sheisc@163.com>
2024-05-28 15:16:32 +1000

    Add Stacks/Recursion

---
+++ Stacks/Recursion/src/main.c
@@ -0,0 +1,37 @@
+/****************************************************************************************
+                Recursive functions and call stack
+
+    1.  Observe the stack frames when calling a recursive function
+
+    2.  Different versions/instances of the local variable 'n' in Factorial(n)
+
+    3.  A Linux Process's memory layout (userspace)
+
+                                                                    COMP9024 24T2
+
+ ******************************************************************************************/
+
+#include <stdio.h>     // printf(), scanf()
+
+long Factorial(long n) {
+    printf("Factorial(): n = %ld, &n = %p\n", n, &n);
+    if (n <= 1) {
+        // the base case for the recursive function
+        return 1;
+    } else {
+        long result = Factorial(n - 1);
+        result = result * n;
+        return result;
+    }
+}
+
+int main(void) {
+    long n = 4;
+    printf("main(): n = %ld, &n = %p\n", n, &n);
+
+    long x = Factorial(n);
+
+    printf("\nFactorial(%ld) = %ld\n", n, x);    
+
+    return 0;
+}
