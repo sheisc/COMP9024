75662db84a0e5276a2741308ebef23349b4331a6
iron <sheisc@163.com>
2024-08-01 13:29:40 +1000

    ADD C/SimpleWebServer

---
+++ C/SimpleWebServer/README.md
@@ -0,0 +1,227 @@
+# SimpleWebServer
+
+``` sh
+/*******************************************************************
+                          Simple Web Server
+
+    1.  How a data stack can be used to store thread IDs in a multi-threaded program
+
+    2.  How to use a mutex lock to protect shared variables
+
+    3.  How to implement a simple web server in C
+
+                                             COMP9024 24T2
+
+ *******************************************************************/
+``` 
+**This project is designed for those looking to enhance their C programming skills during the holidays.**
+
+In this project, we will discuss how a [data stack](../../Stacks/Stack_LL/README.md) can be used to store thread IDs in a multi-threaded program and how to implement a simple web server in C.
+
+[HashMap](../../Strings/HashMap/README.md) can be used to store the HTTP headers as follows.
+
+#### An HTTP request from the Chrome browser
+
+```sh
+GET / HTTP/1.1
+Host: localhost:8080
+User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7
+Accept-Encoding: gzip, deflate, br
+Accept-Language: en-GB,en-US;q=0.9,en;q=0.8
+
+...
+
+```
+
+If you're interested, after completing COMP9024, you can apply the knowledge gained in Database Systems to further improve this simple web server.
+
+Additionally, incorporating a thread pool can further boost the web server's performance.
+
+
+
+### Two fundamental principles in software design
+
+#### High Cohesion
+
+The parts of a program that are grouped together (like a function, class or module) are highly related and focused on a single task.
+
+This makes each part easier to understand and maintain because it has a clear, specific role. 
+
+#### Low Coupling
+
+Minimizing dependencies between different parts of a program.
+
+When different parts of a program (like functions, classes or modules) are loosely connected, changes in one part don’t heavily affect others. 
+
+This makes the program more flexible and easier to update. 
+
+## 1 How to download this project in [CSE VLAB](https://vlabgateway.cse.unsw.edu.au/)
+
+Open a terminal (Applications -> Terminal Emulator)
+
+```sh
+
+$ git clone https://github.com/sheisc/COMP9024.git
+
+$ cd COMP9024/C/SimpleWebServer
+
+SimpleWebServer$ 
+
+```
+
+
+## 2 How to start [Visual Studio Code](https://code.visualstudio.com/) to browse/edit/debug a project.
+
+```sh
+
+SimpleWebServer$ code
+
+```
+
+Two configuration files (SimpleWebServer/.vscode/[launch.json](https://code.visualstudio.com/docs/cpp/launch-json-reference) and SimpleWebServer/.vscode/[tasks.json](https://code.visualstudio.com/docs/editor/tasks)) have been preset.
+
+
+
+#### 2.1 Open the project in VS Code
+
+In the window of Visual Studio Code, please click "File" and "Open Folder",
+
+select the folder "COMP9024/C/SimpleWebServer", then click the "Open" button.
+
+
+#### 2.2 Build the project in VS Code
+
+click **Terminal -> Run Build Task**
+
+
+#### 2.3 Debug the project in VS Code
+
+Open src/WebServer.c, and click to add a breakpoint (say, line 170).
+
+Then, click **Run -> Start Debugging**
+
+### 2.4 Directory
+
+```sh
+├── Makefile             defining set of tasks to be executed (the input file of the 'make' command)
+|
+├── README.md            introduction to this tutorial
+|
+├── src                  containing *.c and *.h
+|   |
+│   ├── WebServer.c      a simple multi-threaded web server
+|   |
+│   ├── Stack.c          For recording thread IDs
+│   ├── Stack.h
+|   |
+│   └── main.c           main()
+|
+|── images               *.png files
+|
+└── .vscode              containing configuration files for Visual Studio Code
+    |
+    ├── launch.json      specifying which program to debug and with which debugger,
+    |                    used when you click "Run -> Start Debugging"
+    |
+    └── tasks.json       specifying which task to run (e.g., 'make' or 'make clean')
+                         used when you click "Terminal -> Run Build Task" or "Terminal -> Run Task"
+```
+
+Makefile is discussed in [COMP9024/C/HowToMake](../../C/HowToMake/README.md).
+
+
+## 3 The main procedure
+
+
+### 3.1 Build and run the program from the command line interface
+
+
+``` sh
+
+SimpleWebServer$ make
+
+SimpleWebServer$ ./main
+
+
+```
+
+### 3.2 Use nc as the client
+
+
+``` sh
+
+$ echo -e "GET / HTTP/1.1\nConnection: close\n\n" | nc localhost 8080
+
+```
+
+#### HTTP request from nc
+
+```sh
+GET / HTTP/1.1
+Connection: close
+
+
+```
+
+#### HTTP response from the simple web server
+
+```sh
+HTTP/1.1 200 OK
+Server: Apache
+Connection: close
+Content-Type: text/html
+Content-Length: 134
+
+<html>
+<head>
+<meta http-equiv="refresh" content="1"></head>
+<body>
+<h1> Hello. Your ticket number is 00000015. </h1>
+</body>
+</html>
+```
+
+#### 404 Not Found
+
+```sh
+echo -e "GET /xxx HTTP/1.1\nConnection: close\n\n" | nc localhost 8080
+
+HTTP/1.1 404 Not Found
+Server: Apache
+Connection: close
+Content-Type: text/html
+Content-Length: 70
+
+<html>
+<head>
+</head>
+<body>
+<h1> 404 Not Found </h1>
+</body>
+</html>
+
+```
+
+### 3.3 Use a browser as the client
+
+http://localhost:8080
+
+#### HTTP request from the Chrome browser
+
+```sh
+GET / HTTP/1.1
+Host: localhost:8080
+User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7
+Accept-Encoding: gzip, deflate, br
+Accept-Language: en-GB,en-US;q=0.9,en;q=0.8
+
+...
+
+```
+#### Display the HTTP response from the web server
+| HTML page | 
+|:-------------:|
+| <img src="images/SimpleWebServer.png" width="80%" height="80%"> |
+
---
+++ C/SimpleWebServer/src/WebServer.c
@@ -0,0 +1,232 @@
+/********************************************************************************
+            How to Use
+
+    How a data stack can be used to store thread IDs in a multi-threaded program.
+
+    1.
+        SimpleWebServer$ make
+
+        SimpleWebServer$ ./main
+    
+    2.  Use nc as the client
+
+        $ echo -e "GET / HTTP/1.1\nConnection: close\n\n" | nc localhost 8080
+
+        HTTP/1.1 200 OK
+        Server: Apache
+        Connection: close
+        Content-Type: text/html
+        Content-Length: 134
+
+        <html>
+        <head>
+        <meta http-equiv="refresh" content="1"></head>
+        <body>
+        <h1> Hello. Your ticket number is 00000015. </h1>
+        </body>
+        </html>
+
+
+    3.  Open a browser as the client 
+
+        http://localhost:8080
+
+                                                            COMP9024
+
+ ***********************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h> //htons()
+#include <string.h>    //memset()
+#include <fcntl.h>     //fcntl()
+#include <pthread.h>
+#include <sys/time.h>
+#include "Stack.h"
+
+#define SERVER_PORT     8080
+//#define MAX_PENDING     10
+#define BUFFER_SIZE     1023
+
+// mutual exclusive
+static pthread_mutex_t counterMutex = PTHREAD_MUTEX_INITIALIZER;
+// ticket counter
+static long counter = 0;
+
+// mutual exclusive
+static pthread_mutex_t stackMutex = PTHREAD_MUTEX_INITIALIZER;
+
+struct Stack *pStack;
+
+static void write_one_line(int client_sock, char *msg) {
+    write(client_sock, msg, strlen(msg));
+    write(client_sock, "\r\n", 2);
+}
+
+
+static void do_error(const char *msg, ...) {
+    perror(msg);
+    if (pStack) {
+        ReleaseStack(pStack);
+    }
+    exit(-1);
+}
+
+#define CONTENT_LENGTH_HEADER  128
+
+/*
+    Chrome will send two HTTP requests:
+    
+    Favorite icon 
+
+        GET /favicon.ico HTTP/1.1
+
+    Default URL
+
+        GET / HTTP/1.1
+
+    We only handle the default URL in this simple web server.
+ */
+static void *run(void *arg) {    
+    int client_sock = (int) ((long) arg);
+    char lengthHeader[CONTENT_LENGTH_HEADER];
+    char responseBuf[BUFFER_SIZE];
+    char inputBuf[BUFFER_SIZE];
+    char *defaultRequest = "GET / HTTP/1.1";
+
+    memset(inputBuf, 0, BUFFER_SIZE);
+    read(client_sock, inputBuf, BUFFER_SIZE);
+    int isDefaultURL = 0;
+    if (strncmp(inputBuf, defaultRequest, strlen(defaultRequest)) == 0) {
+        isDefaultURL = 1;
+    } 
+    
+    long ticketNum = 0;       
+
+    // 1. HTTP response line
+    if (isDefaultURL) {
+        write_one_line(client_sock, "HTTP/1.1 200 OK");
+        pthread_mutex_lock(&counterMutex);
+        counter++;
+        ticketNum = counter;
+        pthread_mutex_unlock(&counterMutex); 
+    } else {
+        write_one_line(client_sock, "HTTP/1.1 404 Not Found");
+    }
+
+    // 2. HTTP response headers
+    write_one_line(client_sock, "Server: Apache");
+    write_one_line(client_sock, "Connection: close");
+    write_one_line(client_sock, "Content-Type: text/html");
+    if (isDefaultURL) {
+        snprintf(responseBuf, BUFFER_SIZE, "<html>\n"
+                    "<head>\n"
+                    "<meta http-equiv=\"refresh\" content=\"1\">"
+                    "</head>\n"
+                    "<body>\n"
+                    "<h1> Hello. Your ticket number is %08ld. </h1>\n"
+                    "</body>\n"
+                    "</html>\n", ticketNum);
+    } else {
+        snprintf(responseBuf, BUFFER_SIZE, "<html>\n"
+                    "<head>\n"
+                    "</head>\n"
+                    "<body>\n"
+                    "<h1> 404 Not Found </h1>\n"
+                    "</body>\n"
+                    "</html>\n");        
+    }
+    snprintf(lengthHeader, CONTENT_LENGTH_HEADER, "Content-Length: %d", (int) strlen(responseBuf));
+    write_one_line(client_sock, lengthHeader);
+    write(client_sock, "\r\n", 2);
+
+    // 3. HTTP response body
+    write_one_line(client_sock, responseBuf);
+    // close the socket
+    close(client_sock);
+
+    pthread_t tid = pthread_self();
+
+    pthread_mutex_lock(&stackMutex);    
+#if 0
+    // Output the HTTP request from the browser
+    printf("%s\n", inputBuf);
+#endif    
+    StackPush(pStack, tid);
+    pthread_mutex_unlock(&stackMutex);
+
+    return NULL;
+}
+
+int main(void) {
+    int serv_sock, client_sock;
+    struct sockaddr_in server_addr, client_addr;
+    // create a data stack to store the thread IDs of the threads that have finished
+    pStack = CreateStack();
+
+    if ((serv_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        do_error("socket() failed");
+    }
+    
+    /*
+    SO_REUSEADDR
+
+        https://stackoverflow.com/questions/3229860/what-is-the-meaning-of-so-reuseaddr-setsockopt-option-linux
+     */
+    int enabled = 1;
+    if( setsockopt(serv_sock, SOL_SOCKET, SO_REUSEADDR, &enabled, sizeof(int)) < 0){
+        do_error("setsockopt() failed");
+	}    
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);    
+    server_addr.sin_port = htons(SERVER_PORT);
+
+    if (bind(serv_sock, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
+        do_error("bind() failed");
+    }
+
+    if (listen(serv_sock, SOMAXCONN) < 0) {
+        do_error("listen() failed");
+        return -1;
+    }
+
+    unsigned int addr_len = sizeof(client_addr);    
+    while (1) {
+
+        client_sock = accept(serv_sock, (struct sockaddr *)&client_addr, &addr_len);
+        if (client_sock < 0) {
+            perror("accept() failed");
+            continue;
+        }
+        // Handle the HTTP request in a worker thread
+        pthread_t worker;
+        if (pthread_create(&worker, NULL, run, (void *) ((long) client_sock)) != 0) {
+            do_error("pthread_create() failed");
+        }
+        // call pthread_join() to do some clean-up operations for the threads that have finished
+        pthread_mutex_lock(&stackMutex);           
+        while (!StackIsEmpty(pStack)) {
+            pthread_t tid = StackPop(pStack);
+            pthread_join(tid, NULL);
+        }
+#if 0
+        char client_name[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &client_addr.sin_addr.s_addr, client_name, sizeof(client_name));
+        printf("Hello: %s/%d\n", client_name, ntohs(client_addr.sin_port));
+#endif        
+        pthread_mutex_unlock(&stackMutex);
+    }
+
+    ReleaseStack(pStack);
+    return 0;
+}
+
+
+
